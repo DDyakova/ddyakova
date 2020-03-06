7ded0939cec5b3916fc63ddd2dca16717283b002
PepoDyakov <ddiakova@gmail.com>
2020-03-06 14:03:49 +0200

    Updates

---
+++ precache-manifest.7e4355ac7228623cdde31dd37242bdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbbf20feb110109660a15f0fff8c179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6cc20099b372498251",
+    "url": "/static/css/main.e00aafb4.chunk.css"
+  },
+  {
+    "revision": "9faed68f68e3ab466a9b",
+    "url": "/static/js/2.50601a65.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.50601a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6cc20099b372498251",
+    "url": "/static/js/main.4dbdff3f.chunk.js"
+  },
+  {
+    "revision": "98f2ed0a8880e7bbdbcb",
+    "url": "/static/js/runtime-main.b14867be.js"
+  }
+]);
