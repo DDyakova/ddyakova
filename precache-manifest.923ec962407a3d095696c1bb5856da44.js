9efc581998efe462b5f875a589ef94c9af2626e8
PepoDyakov <ddiakova@gmail.com>
2020-03-09 12:44:04 +0200

    Updates

---
+++ precache-manifest.923ec962407a3d095696c1bb5856da44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8739e6dc3410666e9d17bdd101f10c",
+    "url": "/ddyakova/index.html"
+  },
+  {
+    "revision": "bf6cc20099b372498251",
+    "url": "/ddyakova/static/css/main.e00aafb4.chunk.css"
+  },
+  {
+    "revision": "9faed68f68e3ab466a9b",
+    "url": "/ddyakova/static/js/2.50601a65.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ddyakova/static/js/2.50601a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6cc20099b372498251",
+    "url": "/ddyakova/static/js/main.4dbdff3f.chunk.js"
+  },
+  {
+    "revision": "5b2758436d34e68f4d2c",
+    "url": "/ddyakova/static/js/runtime-main.d33a97b9.js"
+  }
+]);
